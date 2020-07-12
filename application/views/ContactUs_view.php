d13cab51f2b91d7fb7fad4f94ef82dfe99a103cc
Damian <Damian@DESKTOP-E4AAR0H>
2020-07-11 20:07:59 -0600

    Proyecto Final Multimedios3

--- application/views/ContactUs_view.php
+++ application/views/ContactUs_view.php
@@ -1,17 +1,69 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        // put your code here
-        ?>
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0,miximum-scale=1.0, minimum-scale=1.0">
+    <title>Contact Us</title>
+
+<!--    <link rel="shortcut icon" href="favicon.ico" type="image/ico">-->
+
+<!--    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>-->
+
+ <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  
+  
+<!--<link type="text/css" rel="stylesheet" href="assets/css/estilos.css" />-->
+<link rel="stylesheet" href="<?php echo base_url('assets/css/estilos.css'); ?>"/>
+     <body>
+       
+         <section class="container">
+    <div class="row">
+      <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
+        <div class="card card-signin my-5">
+          <div class="card-body">
+            <h5 class="card-title text-center">Contact Us</h5>
+            <form class="form-signin" method="post" action="<?=site_url('CrearCitaSinRegistro_controller/crearCita')?>">
+                 <div class="form-label-group">
+                   
+                    <label for="inputName">Full Name</label>
+                <input type="name" name="name" class="form-control" placeholder="full name" required>
+               
+              </div>
+                
+                <div class="form-label-group">
+                  
+                        
+                <label for="inputEmail">E-mail Address</label>
+                <input type="email" name="email" class="form-control" placeholder="E-mail Address" required autofocus>
+                
+              </div>
+
+          
+              
+              <div class="form-label-group">
+                 <label for="message">Message</label>
+                 <textarea  type="text" name="message" class="form-control" placeholder="Write here..." required></textarea> 
+               
+              </div>
+               
+                <button class="btn btn-primary" type="submit">Continue</button>
+            </form>
+             <a href="<?=site_url('')?>"><button class="btn btn-primary">Cancelar</button></a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+     
+       
     </body>
 </html>
